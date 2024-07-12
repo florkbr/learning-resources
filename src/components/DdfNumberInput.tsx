fcedc1d13b5f5be6b7041d89f5ff2ba6664fbbcd
Janet Cobb <jacobb@redhat.com>
2024-07-12 15:51:14 -0400

    Handle focus events in DdfNumberInput

--- src/components/DdfNumberInput.tsx
+++ src/components/DdfNumberInput.tsx
@@ -1,14 +1,28 @@
-import { FormGroup, NumberInput } from '@patternfly/react-core';
+import {
+  FormGroup,
+  FormHelperText,
+  HelperText,
+  HelperTextItem,
+  NumberInput,
+} from '@patternfly/react-core';
 import React, { useId } from 'react';
 import {
   UseFieldApiConfig,
   useFieldApi,
 } from '@data-driven-forms/react-form-renderer';
+import ExclamationCircleIcon from '@patternfly/react-icons/dist/dynamic/icons/exclamation-circle-icon';
 
 const DdfNumberInput = (props: UseFieldApiConfig) => {
-  const { input, ...rest } = useFieldApi(props);
+  const { input, meta, ...rest } = useFieldApi(props);
   const reactId = useId();
   const effectiveId = rest.id || reactId;
+
+  const showInvalid = meta.touched && meta.invalid;
+
+  const focusProps = {
+    onFocus: input.onFocus,
+    onBlur: input.onBlur,
+  };
 
   return (
     <FormGroup
@@ -20,12 +34,26 @@ const DdfNumberInput = (props: UseFieldApiConfig) => {
         id={effectiveId}
         inputName={input.name}
         value={input.value}
+        validated={showInvalid ? 'error' : 'default'}
         min={rest.minValue}
+        unit={rest.unit}
         onPlus={() => input.onChange((input.value ?? 0) + 1)}
         onMinus={() => input.onChange((input.value ?? 0) - 1)}
         onChange={(e) => input.onChange(e.currentTarget.valueAsNumber)}
-        unit={rest.unit}
+        {...focusProps}
+        plusBtnProps={focusProps}
+        minusBtnProps={focusProps}
       />
+
+      {showInvalid ? (
+        <FormHelperText>
+          <HelperText>
+            <HelperTextItem variant={'error'} icon={<ExclamationCircleIcon />}>
+              {meta.error}
+            </HelperTextItem>
+          </HelperText>
+        </FormHelperText>
+      ) : null}
     </FormGroup>
   );
 };
