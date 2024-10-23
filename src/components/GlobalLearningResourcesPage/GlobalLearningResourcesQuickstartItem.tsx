a141fd935469c6c4606ad7de66833a0e2a2f4e2e
Justin Orringer <justinorringer@gmail.com>
2024-10-23 14:28:54 -0400

    Changing quick start URL

--- src/components/GlobalLearningResourcesPage/GlobalLearningResourcesQuickstartItem.tsx
+++ src/components/GlobalLearningResourcesPage/GlobalLearningResourcesQuickstartItem.tsx
@@ -38,7 +38,7 @@ const GlobalLearningResourcesQuickstartItem: React.FC<
     quickStart.metadata.favorite
   );
   const quickStartURL = new URL(
-    quickStart.spec.link?.href ?? 'https://access.redhat.com/'
+    quickStart.spec.link?.href ?? 'https://docs.redhat.com/'
   );
   const labelColor = quickStart.spec.type?.color;
   const QUICK_START_TYPE: QuickStartType = {
